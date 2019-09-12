19d974ecf291a87f77ed99ea52f179ab0fb3c231
Minxuan Lin <James.lin1314520@outlook.com>
2019-09-12 10:11:25 +0930

    Implemented delete function, added css to data, improved website function
    Signed-off-by: Minxuan Lin <James.lin1314520@outlook.com>

--- js/js.js
+++ js/js.js
@@ -1,16 +1,34 @@
+document.querySelector('.outputTable').innerHTML = '';
+
+for (var index = 0; index < localStorage.length; index++) {
+  var key = localStorage.key(index);
+  var value = localStorage.getItem(key);
+  console.log(`${key}: ${value}`);
+  showCard(key, value);
+}
+
 function saveDataClicked(evt) {
   evt.preventDefault();
 
-var keyInput = document.querySelector('#keyInput');
+  var keyInput = document.querySelector('#keyInput');
 
-var valueInput = document.querySelector('#valueInput');
+  var valueInput = document.querySelector('#valueInput');
 
-var key = keyInput.value;
-var value = valueInput.value;
+  var key = keyInput.value;
+  var value = valueInput.value;
 
-localStorage.setItem(key, value);
+  localStorage.setItem(key, value);
+  document.querySelector('.outputTable').innerHTML = '';
+
+  for (var index = 0; index < localStorage.length; index++) {
+    var key = localStorage.key(index);
+    var value = localStorage.getItem(key);
+    console.log(`${key}: ${value}`);
+    showCard(key, value);
+  }
 };
 
+/* seperate function
 function loadDataClicked(evt) {
   evt.preventDefault();
   document.querySelector('.outputTable').innerHTML = '';
@@ -19,43 +37,56 @@ for (var index = 0; index < localStorage.length; index++) {
   var key = localStorage.key(index);
   var value = localStorage.getItem(key);
   console.log(`${key}: ${value}`);
-  showRow(key, value);
+  showCard(key, value);
 }
 }
+*/
 
 function deleteClicked(evt) {
-  var key = evt.target.dataset.key;
-  localStorage.removeItem('key');
+  localStorage.removeItem(evt.target.dataset.key);
+  evt.preventDefault();
+  document.querySelector('.outputTable').innerHTML = '';
+
+for (var index = 0; index < localStorage.length; index++) {
+  var key = localStorage.key(index);
+  var value = localStorage.getItem(key);
+  console.log(`${key}: ${value}`);
+  showCard(key, value);
+}
 
 }
 
-function showRow(key, value) {
-  var keyCell = document.createElement('td');
-  keyCell.innerHTML = key;
-  var valueCell = document.createElement('td');
-  valueCell.innerHTML = value;
-  var deleteRow = document.createElement('td');
+function showCard(key, value) {
+
+  var col = document.createElement('td')
+
+  var card = document.createElement('div');
+  card.className = "hpcard";
+
+  var cardHeading = document.createElement('h4');
+  cardHeading.className = "txt_center";
+  cardHeading.innerHTML = key;
+
+  var cardContent = document.createElement('div');
+  cardContent.className = "hpcontainer";
+  cardContent.innerHTML = value;
+
+  var buttonCenter = document.createElement('center');
+
   var deleteButton = document.createElement('button');
-  deleteButton.class = 'delBtn';
-  deleteButton.innerHTML = 'delete';
+  deleteButton.className = 'hpbutton';
+  deleteButton.innerHTML = 'Delete memo';
   deleteButton.dataset.key = key;
   deleteButton.addEventListener('click', deleteClicked);
-  deleteRow.appendChild(deleteButton);
-  var row = document.createElement('tr');
-  row.appendChild(keyCell);
-  row.appendChild(valueCell);
-  row.appendChild(deleteRow);
 
-  document
-    .querySelector('.outputTable')
-    .appendChild(row);
-  return row;
+  buttonCenter.appendChild(deleteButton)
+  card.appendChild(cardHeading);
+  card.appendChild(cardContent);
+  card.appendChild(buttonCenter)
+  col.appendChild(card)
+
+  document.querySelector('.outputTable').appendChild(col);
+  return col;
 }
 
-document
-  .getElementById('saveBtn')
-  .addEventListener('click', saveDataClicked);
-
-document
-  .getElementById('loadBtn')
-  .addEventListener('click', loadDataClicked);
+document.getElementById('saveBtn').addEventListener('click', saveDataClicked);
